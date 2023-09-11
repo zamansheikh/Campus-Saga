fc97cd2acf6bcf666c6e453353b619ce0b5c6159
Zaman Sheikh <zaman6545@gmail.com>
2023-09-12 00:04:07 +0600

    nafiz

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  RxBool isDark = false.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  
+}
