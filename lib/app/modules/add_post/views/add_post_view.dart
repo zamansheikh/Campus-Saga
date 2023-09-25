69bc31bcef317f3b578cb5edcdd911ecde171abd
Nafiz4041 <nafiz15-4041@diu.edu.bd>
2023-09-25 23:41:46 +0600

    add post

--- lib/app/modules/add_post/views/add_post_view.dart
+++ lib/app/modules/add_post/views/add_post_view.dart
@@ -45,111 +45,121 @@ class AddPostView extends GetView<AddPostController> {
         ),
       ),
       body: Container(
-        height: Get.height,
         width: Get.width,
         color: Color(0xFF207BFF),
-        child: Container(
-          child: Column(
-            children: [
-              DropdownButtonFormField(
-                decoration: InputDecoration(
-                  label: Text('Post Type'),
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 38, 38, 38),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+        child: SingleChildScrollView(
+          child: Expanded(
+            child: Container(
+              padding:
+                  EdgeInsets.only(left: 12, right: 12, top: 12, bottom: 60),
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.5),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15),
+                  topLeft: Radius.circular(15),
                 ),
-                items: [
-                  DropdownMenuItem(
-                    child: Text('Issue'),
-                    value: 'Issue',
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x19000000),
+                    blurRadius: 20,
+                    offset: Offset(0, -5),
+                    spreadRadius: 2,
+                  )
+                ],
+              ),
+              child: Column(
+                children: [
+                  DropdownButtonFormField(
+                    decoration: InputDecoration(
+                      label: Text('Post Type'),
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 38, 38, 38),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('Issue'),
+                        value: 'Issue',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Promotional'),
+                        value: 'Promotional',
+                      ),
+                    ],
+                    onChanged: (value) {},
                   ),
-                  DropdownMenuItem(
-                    child: Text('Promotional'),
-                    value: 'Promotional',
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    maxLength: 100,
+                    decoration: InputDecoration(
+                      label: Text('Tilte'),
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 38, 38, 38),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      label: Text('Description'),
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 38, 38, 38),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.all(10),
+                    height: 250,
+                    width: Get.width,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x19000000),
+                          blurRadius: 8,
+                          offset: Offset(0, 1),
+                          spreadRadius: 2,
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Post'),
                   ),
                 ],
-                onChanged: (value) {},
               ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                maxLength: 100,
-                decoration: InputDecoration(
-                  label: Text('Tilte'),
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 38, 38, 38),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                maxLines: 5,
-                decoration: InputDecoration(
-                  label: Text('Description'),
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 38, 38, 38),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5),
-                alignment: Alignment.topLeft,
-                padding: EdgeInsets.all(10),
-                height: 250,
-                width: Get.width,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.5),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0x19000000),
-                      blurRadius: 8,
-                      offset: Offset(0, 1),
-                      spreadRadius: 2,
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-          padding: EdgeInsets.all(12),
-          height: Get.height,
-          width: Get.width,
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.5),
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(15),
-              topLeft: Radius.circular(15),
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x19000000),
-                blurRadius: 20,
-                offset: Offset(0, -5),
-                spreadRadius: 2,
-              )
-            ],
           ),
         ),
       ),
